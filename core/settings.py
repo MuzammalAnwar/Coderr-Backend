41d60b8598a9b3915e25ceb5d4cf68dd1de9982b
Muzammal Anwar <muzammalanwar556@gmail.com>
2025-09-17 14:34:42 +0200

    activated default auth class as TokenAuthentication in settings.py

--- core/settings.py
+++ core/settings.py
@@ -133,9 +133,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.TokenAuthentication'
-    # ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
 }
 
 AUTH_USER_MODEL = "auth_app.User"
